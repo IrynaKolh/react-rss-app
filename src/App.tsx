f9c57bb5e9313e4b6a488d90486d98bcb4efd473
Iryna <iryna.kolhanova@gmail.com>
2023-03-22 21:30:30 -0400

    fix: route after migrate to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import HomePage from './pages/HomePage';
-import NotFound from './pages/NotFoundPage';
-import React from 'react';
+import NotFoundPage from './pages/NotFoundPage';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
-import Navigation from './components/Navigation';
 import FormPage from './pages/FormPage';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
-      <header>
-        <Navigation />
-      </header>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/form" element={<FormPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<FormPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/notFound.css';
+import { Link } from 'react-router-dom';
 
-function NotFound() {
+function NotFoundPage() {
   return (
     <div>
       <h1 className="not-found-page">404! Page not found!</h1>
+      <Link to="/">{'Home page'}</Link>
     </div>
   );
 }
 
-export default NotFound;
+export default NotFoundPage;
