c4535c45d763c152b671f61c3aaaf775c2019320
Iryna <iryna.kolhanova@gmail.com>
2023-03-28 09:30:55 -0400

    refactor: renaming

--- src/test/form.test.tsx
+++ src/test/form.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Form from './../components/Form';
+import Form from '../components/Form';
 import { Character } from './../model/interfases';
 
 const myCallback = (a: Character) => console.log(a);
