7f421b688bf77a71ce303c13ee0f1004d43db954
Iryna <iryna.kolhanova@gmail.com>
2023-04-04 09:58:51 -0400

    fix: home page + search

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,52 +1,57 @@
 import Search from '../components/Search';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Cards from '../components/Cards';
 import './../pages/styles/homePage.css';
 import { URL, SEARCH_PARAM } from '../helpers/constants';
 
 const HomePage = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState(localStorage.getItem('search') ?? '');
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const searchQueryRef = useRef<string>();
 
   const fetchData = (searchQuery: string) => {
     fetch(`${URL}?${SEARCH_PARAM}${searchQuery}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok === false) {
+          throw Error('Server does not response. Try later.');
+        }
+        return res.json();
+      })
       .then((data) => {
         setData(data.results);
         setIsLoading(false);
+        setError(null);
       })
-      .catch((error) => error);
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
   };
 
   useEffect(() => {
-    if (searchQuery) {
-      localStorage.setItem('search', searchQuery);
-    } else {
-      localStorage.removeItem('search');
-    }
+    localStorage.setItem('search', searchQuery);
+    searchQueryRef.current = searchQuery;
     fetchData(searchQuery);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
+    searchQueryRef.current = e.target.value;
   };
 
   const cardsProps = {
     data: data,
   };
-  let searchResult;
-  if (data) {
-    searchResult = <Cards {...cardsProps} />;
-  } else {
-    searchResult = <p>Not found this character... </p>;
-  }
 
   return (
     <div className="home-page-container">
       <Search onChange={handleInputChange} value={searchQuery} />
       {isLoading && <p> Loading...</p>}
-      {searchResult}
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {data ? <Cards {...cardsProps} /> : <p>Not found this character... </p>}
     </div>
   );
 };
